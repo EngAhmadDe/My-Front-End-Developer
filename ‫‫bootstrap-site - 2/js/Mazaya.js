9f4012617e4df43f673d11eed421ab846c462e48
EngAhmadDe <110621768+EngAhmadDe@users.noreply.github.com>
2022-08-14 12:46:11 +0300

    Update Mazaya.js

--- ‫‫bootstrap-site - 2/js/Mazaya.js
+++ ‫‫bootstrap-site - 2/js/Mazaya.js
@@ -22,11 +22,3 @@ function startCount(el) {
   }, 2000 / goal);
 }
 // Counter
-
-
-// loader
-setTimeout(() => {
-  document.getElementById("loader").style.display = "none";
-  document.body.style.overflow = "visible";
-}, "6000");
-
