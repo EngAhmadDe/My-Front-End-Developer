c9f1af3bc49c6ee2a9485adaea34bb0f65368106
EngAhmadDe <110621768+EngAhmadDe@users.noreply.github.com>
2022-08-17 19:16:53 +0300

    Update Mazaya.js

--- mazaya1/js/Mazaya.js
+++ mazaya1/js/Mazaya.js
@@ -23,3 +23,27 @@ function startCount(el) {
 }
 // Counter
 
+// percentage
+let circleSection = document.querySelector(".steps");
+let circles = document.querySelectorAll(".circle-progress");
+let progressCircles = document.querySelectorAll(".progress");
+let progressText = document.querySelectorAll(".circles-progress h1");
+let bol = false;
+
+window.addEventListener("scroll", function () {
+  if (pageYOffset > circleSection.offsetTop - 800 && bol === false) {
+    for (let i = 0; i < circles.length; i++) {
+      let radius = progressCircles[i].r.baseVal.value;
+      let circumference = radius * 2 * Math.PI;
+      progressCircles[i].style.strokeDasharray = circumference;
+      function setProgress(percent) {
+        progressCircles[i].style.strokeDashoffset =
+          circumference - (percent / 100) * circumference;
+      }
+      let progress = circles[i].dataset.prog;
+      progressText[i].innerHTML = progress + "%";
+      setProgress(progress);
+      bol = true;
+    }
+  }
+});
